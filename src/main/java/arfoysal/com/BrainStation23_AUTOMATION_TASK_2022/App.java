2790fe081fd8babe3260ec8fccf8d058ce438f61
arfoysal <arfoysal.me@gmail.com>
2022-02-14 21:32:23 +0600

    Add driver package and pom.xml file

---
+++ src/main/java/arfoysal/com/BrainStation23_AUTOMATION_TASK_2022/App.java
@@ -0,0 +1,13 @@
+package arfoysal.com.BrainStation23_AUTOMATION_TASK_2022;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
