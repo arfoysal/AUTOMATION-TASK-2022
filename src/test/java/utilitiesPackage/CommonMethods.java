8cb320feb37402d70f278a5b150b7a1e0f5dceba
arfoysal <arfoysal.me@gmail.com>
2022-02-17 23:39:55 +0600

    optimized code for CommonMethods and Take_Screenshot class

--- src/test/java/utilitiesPackage/CommonMethods.java
+++ src/test/java/utilitiesPackage/CommonMethods.java
@@ -1,16 +1,11 @@
 package utilitiesPackage;
 
 import java.time.Duration;
-import java.util.List;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -22,7 +17,6 @@ public class CommonMethods {
 	 * Get Current Page URL * 
 	 * **********************
 	 */
-
 	public String currentPageUrl() {
 		return PageDriver.getCurrentDriver().getCurrentUrl();
 	}
@@ -31,168 +25,61 @@ public class CommonMethods {
 	 * Get Current Page Title * 
 	 * ************************
 	 */
-	
 	public String getTitle() {
 		return PageDriver.getCurrentDriver().getTitle();
 	}
 	
 	/**************************
-	 * Write in a input field * 
+	 * Write in a input field 
+	 * @param element This is the target element where to write
+	 * @param value what to write
 	 * ************************
 	 */
-
 	public void sendText(WebElement element, String value) {
 		element.clear();
 		element.sendKeys(value);
 	}
 	
 	/******************************
-	 * Perform Hover on a element * 
+	 * Perform Hover on an element 
+	 * @param element This is the target element where to perform hover action
 	 * ****************************
 	 */
-
 	public void hover(WebElement element) {
 		Actions actions = new Actions(PageDriver.getCurrentDriver());
 		actions.clickAndHold(element).build().perform();
 	}
 	
-	/***********************************
-	 * Handle shadow Dome Related Task * 
-	 * *********************************
+	/*********************************************
+	 * Handle Select Tag Related Element Task    *
+	 * @param element locator of Select tag      *
+	 * @param value attribute value of option tag* 
+	 * *******************************************
 	 */
-
-	public void shadowDomePopUp(WebElement host, By btn) {
-		// Finding the ShadowRoot in a WebElement
-		SearchContext context = host.getShadowRoot();
-		// finding the targeted element
-		context.findElement(btn).click();
-	}
-	
-	/******************************************
-	 * Handle Select Tag Related Element Task * 
-	 * ****************************************
-	 */
-
 	public void selectitem(WebElement element, String value) {
 		Select selectItems = new Select(element);
 		selectItems.selectByValue(value);
 	}
 
-	public String selecteditemText(WebElement element) {
-		Select selectItems = new Select(element);
-		return selectItems.getFirstSelectedOption().getText();
-	}
-
-	public Boolean itemSelectStatus(List<WebElement> elements, String value) {
-		for (WebElement element : elements) {
-			if (element.getAttribute("value").equals(value)) {
-				return element.isSelected();
-			}
-		}
-		return false;
-	}
-	
-	/*************************************
-	 * Handle List elements Related Task * 
-	 * ***********************************
+	/*********************************************
+	 * Scroll to an element
+	 * @param element This is the target element * 
+	 * *******************************************
 	 */
-	
-	public void clickOnNthNumberButton(List<WebElement> elements, int position) {
-		elements.get(position - 1).click();
-	}
-	
-	public void listitem(List<WebElement> elements, String visibleText) {
-		for (WebElement element : elements) {
-			if (element.getText().equals(visibleText)) {
-				element.click();
-				break;
-			}
-		}
-	}
-	
-	/**********************************
-	 * Handle JavaScript Related Task * 
-	 * ********************************
-	 */
-
-	public void clickOnAButtonJs(String script) {
-		JavascriptExecutor js = (JavascriptExecutor) PageDriver.getCurrentDriver();
-		js.executeScript(script + ".click();");
-	}
-
-	public void highlighter(WebElement element) {
-		JavascriptExecutor js = (JavascriptExecutor) PageDriver.getCurrentDriver();
-		js.executeScript("arguments[0].setAttribute('style','border:2px solid red;background: beige');", element);
-	}
-
 	public void scrollToElement(WebElement element) {
 		JavascriptExecutor js = (JavascriptExecutor) PageDriver.getCurrentDriver();
 		js.executeScript("arguments[0].scrollIntoView(true);", element);
 	}
 
-	public void scrollInsideElement(String cssLocator, int offsetX, int offsetY) {
-		JavascriptExecutor js = (JavascriptExecutor) PageDriver.getCurrentDriver();
-		js.executeScript("document.querySelector('" + cssLocator + "').scrollBy(" + offsetX + "," + offsetY + ");");
-	}
-
-	/****************************
-	 * Handle Wait Related Task * 
-	 * **************************
+	/********************************************
+	 * Wait for completely load current web page* 
+	 * ******************************************
 	 */
 	WebDriverWait wait;
-
-	public void timeOut() {
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void timeOut(int duretionMS) {
-		try {
-			Thread.sleep(duretionMS);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public Alert waitForAlert() {
-		wait = new WebDriverWait(PageDriver.getCurrentDriver(), Duration.ofSeconds(30));
-		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-		return alert;
-	}
-
-	public void waitForElement(By locator) {
-		wait = new WebDriverWait(PageDriver.getCurrentDriver(), Duration.ofSeconds(30));
-		wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-	}
-
 	public void waitForPageLoad() {
 		wait = new WebDriverWait(PageDriver.getCurrentDriver(), Duration.ofSeconds(30));
 		wait.until((ExpectedCondition<Boolean>) wd -> ((JavascriptExecutor) wd)
 				.executeScript("return document.readyState;").equals("complete"));
-	}
-
-	/*****************************
-	 * Handle Alert Related Task * 
-	 * ***************************
-	 */
-
-	public void alertAccept() {
-		Alert alert = waitForAlert();
-		alert.accept();
-	}
-
-	public void allertAccept(String value) {
-		Alert alert = waitForAlert();
-		alert.sendKeys(value);
-		alert.accept();
-	}
-
-	public void alertDismiss() {
-		Alert alert = waitForAlert();
-		alert.dismiss();
 	}
 
 }
--- src/test/java/utilitiesPackage/Take_Screenshot.java
+++ src/test/java/utilitiesPackage/Take_Screenshot.java
@@ -1,16 +1,9 @@
 package utilitiesPackage;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebElement;
 
 import driverPackage.PageDriver;
 import io.qameta.allure.Allure;
@@ -18,29 +11,10 @@ import io.qameta.allure.Step;
 
 public class Take_Screenshot {
 
-	public void screenshot(String name) throws IOException {
-		String strtime = setdate();
-		File scFile = ((TakesScreenshot) PageDriver.getCurrentDriver()).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(scFile, new File("./image/" + strtime + "_" + name + ".png"));
-	}
-
-	public void screenshot(String name, WebElement element) throws IOException {
-		String strtime = setdate();
-		File scFile = element.getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(scFile, new File("./image/" + strtime + name + ".png"));
-	}
-
 	@Step("Taking a screenshot for {0}")
 	public static void takeScreenShot(String name) {
 		Allure.addAttachment(name, new ByteArrayInputStream(
 				((TakesScreenshot) PageDriver.getCurrentDriver()).getScreenshotAs(OutputType.BYTES)));
-	}
-
-	public String setdate() {
-		Date date = Calendar.getInstance().getTime();
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyymmdd_HHmmss");
-		String strdate = formatter.format(date);
-		return strdate;
 	}
 
 }
